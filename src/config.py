b23afcf436e1b36d61d30a3552bf5f65e52826d9
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-07-14 00:09:35 +0700

    # This is a combination of 3 commits. # This is the 1st commit message:
    Sample data
    
    # This is the commit message #2:
    
    Update dataset import
    
    # This is the commit message #3:
    
    Dataset

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ data_path = path.realpath(path.join(__file__, path.pardir, path.pardir, "data"))
 x_path = str(path.realpath(path.join(data_path, "image.npy")))
 y_path = str(path.realpath(path.join(data_path, "label.npy")))
 
+import_batch = 1000
 test_size = 0.2
 kfold_nsplits = 10
 kfold_nrepeats = 10
--- src/dataset_import.py
+++ src/dataset_import.py
@@ -1,56 +1,108 @@
 import os
 from os import path
-from typing import Tuple
+from glob import glob
+import csv
+import cv2
 import numpy as np
 import torch
 from torchvision import transforms
-from npy_append_array import NpyAppendArray
+from npy_append_array import NpyAppendArray as BigArray
 import config
-dataset_path = str(path.realpath(path.join(config.data_path, "RAW")))
 
-filenames = ["VID1", "VID3", "VID4", "VID5", "VID6"]
+dataset_path = str(path.realpath(path.join(config.data_path, "RAW")))
+video_file_names = ["VID1", "VID3", "VID4", "VID5", "VID6"]
+image_folder_names = ["video_10", "video_11", "video_20", "video_40",
+                      "video_41", "video_60", "video_110", "video_120", "video_130"]
 
 use_cuda = torch.cuda.is_available()
 if not use_cuda:
     print("CUDA not used!")
 device = torch.device("cuda" if use_cuda else "cpu")
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
-    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-])
+'''Transformation list'''
+transform = transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
 
-tensor_transform = transforms.ToTensor()
-
-def transform_array(img_batch: torch.FloatTensor) -> torch.FloatTensor:
-    img_batch = img_batch.to(device)
-    img_batch /= 255.0
-    img_batch = transform(img_batch)
-    img_batch = img_batch.cpu().detach()
-    return img_batch
+def transform_array(image_batch: torch.FloatTensor) -> torch.FloatTensor:
+    '''Transform an array of image'''
+    image_batch = image_batch.to(device)
+    image_batch /= 255.0
+    image_batch = transform(image_batch)
+    image_batch = image_batch.cpu().detach()
+    return image_batch
 
 
-def split_append_array(total_img: NpyAppendArray, img: np.ndarray) -> None:
-    per_batch = 1000
-    image_count = img.shape[0]
+def split_append_array(total_image: BigArray, image: np.ndarray) -> None:
+    per_batch = config.import_batch
+    image_count = image.shape[0]
     for i in range(0, image_count, per_batch):
-        last_idx = min(i + 1000, img.shape[0])
-        img_batch = img[i:last_idx]
+        last_idx = min(i + per_batch, image.shape[0])
+        image_batch = image[i:last_idx]
 
-        img_batch = img_batch.astype(float)
-        img_batch = np.transpose(img_batch, (0, 3, 1, 2))
-        img_batch = torch.from_numpy(img_batch)
-        img_batch = transform_array(img_batch).numpy()
-        
-        img_batch = np.ascontiguousarray(img_batch)
-        total_img.append(img_batch)
+        image_batch = image_batch.astype(float)
+        image_batch = np.transpose(image_batch, (0, 3, 1, 2))
+        image_batch = torch.from_numpy(image_batch)
+        image_batch = transform_array(image_batch).numpy()
+
+        image_batch = np.ascontiguousarray(image_batch)
+        total_image.append(image_batch)
 
 
-def get_path(filename: str) -> 'Tuple(str, str)':
-    return (
-        path.join(dataset_path, f"{filename}_Extract.npy"),
-        path.join(dataset_path, f"{filename}.npy")
-    )
+def load_video(total_image: BigArray, video_name: str) -> None:
+
+    image_path = path.join(dataset_path, "image", f"{video_name}_Extract.npy")
+
+    print(f"Loading {video_name}")
+    image_array = np.load(image_path)
+    assert((image_array.shape[1], image_array.shape[2]) == (224, 224))
+
+    print(f"Processing {video_name}")
+    split_append_array(total_image, image_array)
+
+
+def load_video_label(total_label: BigArray, video_name: str) -> None:
+    label_path = path.join(dataset_path, "label", f"{video_name}.npy")
+
+    print(f"Loading label {video_name}")
+    label_array = np.load(label_path)
+    total_label.append(label_array)
+
+
+def load_image_folder(total_image: BigArray, folder_name: str) -> None:
+    print(f"Loading {folder_name}")
+    image_paths = glob(path.join(dataset_path, "image", folder_name, "*"))
+    image_paths = sorted(image_paths)
+    image_array = []
+    batch_num = 1
+    for image in image_paths:
+        img = cv2.imread(image)
+        img = cv2.resize(img, (224, 224))
+        image_array.append(img)
+        if len(image_array) == config.batch_size:
+            print(f"Processing batch {batch_num} of {folder_name}: ", end="")
+            batch_num += 1
+            image_array = np.array(image_array)
+            split_append_array(total_image, image_array)
+            image_array = []
+            print("Finished.")
+
+    image_array = np.array(image_array)
+    split_append_array(total_image, image_array)
+
+def load_image_label(total_label: BigArray, folder_name: str) -> None:
+    label_path = path.join(dataset_path, "label", f"{folder_name}.csv")
+    label_array = []
+    print(f"Loading label {folder_name}")
+    with open(label_path, "r", encoding="utf-8") as csv_file:
+        reader = csv.reader(csv_file)
+        first = True
+        for row in reader:
+            if first:
+                first = False
+                continue
+            label_array.append(row[1] == '0') # The dataset is inverted
+    label_array = np.array(label_array)
+    total_label.append(label_array)
+
 
 if __name__ == '__main__':
     if os.path.exists(config.x_path):
@@ -58,16 +110,11 @@ if __name__ == '__main__':
     if os.path.exists(config.y_path):
         os.remove(config.y_path)
 
-    with NpyAppendArray(config.x_path) as array_x:
-        with NpyAppendArray(config.y_path) as array_y:
-            for name in filenames:
-                paths = get_path(name)
-                print(f"Loading {paths[0]}")
-                image_array = np.load(paths[0])
-
-                print(f"Processing {paths[0]}")
-                split_append_array(array_x, image_array)
-
-                print(f"Loading {paths[1]}")
-                current_y = np.load(paths[1])
-                array_y.append(current_y)
+    with BigArray(config.x_path) as total_image:
+        with BigArray(config.y_path) as total_label:
+            for video_name in video_file_names:
+                load_video(total_image, video_name)
+                load_video_label(total_label, video_name)
+            for folder_name in image_folder_names:
+                load_image_folder(total_image, folder_name)
+                load_image_label(total_label, folder_name)
