01819e8cff25f61bc2692b8e774f892c7a25346b
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-07-18 17:36:41 +0000

    Edit custom model

--- src/settings/modules/models.py
+++ src/settings/modules/models.py
@@ -44,27 +44,26 @@ class NoeNet(nn.Module):
         self.grayscale = transforms.Grayscale()
 
         self.increase = nn.Sequential(
-            self.conv(  1,  32), nn.ReLU(), self.mxpool(),
-            self.conv( 32,  64), nn.ReLU(),
-            self.conv( 64, 128), nn.ReLU(), self.mxpool(),
-            self.conv(128, 256), nn.ReLU(),
-            self.conv(256, 512), nn.ReLU(), self.mxpool(),
+            self.conv(  1,  32), nn.ReLU(),
+            self.conv( 32,  64), nn.ReLU(), self.mxpool(),
+            self.conv( 64, 512), nn.ReLU()
         )
+
         self.steady = nn.Sequential(
+            self.conv(512, 512), nn.ReLU(), self.mxpool(),
             self.conv(512, 512), nn.ReLU(),
+            self.conv(512, 512), nn.ReLU(), self.mxpool(),
             self.conv(512, 512), nn.ReLU(),
-            self.conv(512, 512), nn.ReLU(),
-            self.conv(512, 512), nn.ReLU(),
-            self.conv(512, 512), nn.ReLU(),
-            self.mxpool()
+            self.conv(512, 512), nn.ReLU(), self.mxpool()
         )
+
         self.avgpool = nn.AvgPool2d(2, 2)
+
         self.classifier = nn.Sequential(
-            self.lin(512 * 7 * 7, 4096), nn.ReLU(), self.drop(),
-            self.lin(4096, 1024), nn.ReLU(), self.drop(),
+            self.lin(512 * 7 * 7, 1024), nn.ReLU(), self.drop(),
+            self.lin(1024, 1024), nn.ReLU(), self.drop(),
             self.lin(1024, 256), nn.ReLU(), self.drop(),
-            self.lin(256, 32), nn.ReLU(), self.drop(),
-            self.lin(32, 2)
+            self.lin(256, 2)
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -78,3 +77,50 @@ class NoeNet(nn.Module):
 
 def get_custom() -> nn.Module:
     return NoeNet().to(device)
+# class Net(nn.Module):
+#     def __init__(self):
+#         super(Net, self).__init__()
+
+#         self.conv = [
+#             nn.Conv2d(3, 64, 3, padding = 1),
+#             nn.Conv2d(64, 128, 3, padding = 1),
+#             nn.Conv2d(128, 256, 3, padding = 1),
+#             nn.Conv2d(256, 512, 3, padding = 1)
+#         ]
+#         self.conv = nn.ModuleList(self.conv)
+
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.dropout = nn.Dropout(0.3)
+#         self.fc = [
+#             nn.Linear(7 * 7 * 512, 4096),
+#             nn.Linear(4096, 4096),
+#             nn.Linear(4096, 1000)
+#         ]
+#         self.fc = nn.ModuleList(self.fc)
+#         self.final = nn.Linear(1000, 2)
+
+#         self.labels_count = 2
+
+#     def forward(self, inputs):
+#         x = self.pool(F.relu(self.conv[0](inputs)))
+
+#         for i in range(1, len(self.conv)):
+#             x = self.pool(F.relu(self.conv[i](x)))
+
+#         x = self.pool(x)
+#         # print(x.shape)
+
+#         x = x.view(-1, 7 * 7 * 512)
+#         # print(x.shape)
+
+#         for i in self.fc:
+#             x = F.relu(i(x))
+
+#         x = self.dropout(x)
+#         x = self.final(x)
+
+#         preds = nn.LogSoftmax(dim = 1)(x)
+#         return preds
+
+# def get_model2() -> nn.Module:
+#     return Net()
