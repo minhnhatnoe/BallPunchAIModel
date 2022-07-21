84f2001ac6ee969c3ea7397c636e6e0c8a7d69e9
minhnhatnoe <86871862+minhnhatnoe@users.noreply.github.com>
2022-07-21 00:53:15 +0000

    Create solve tolerance

---
+++ src/helper/final_heuristics/solve_tolerance.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define endl "\n"
+
+signed main(){
+    cin.tie(0)->sync_with_stdio(0);
+    freopen("data.txt", "r", stdin);
+    pair<string, ld> tmp;
+    vector<pair<string, ld>> a;
+    while (cin >> tmp.first >> tmp.second){
+        
+    }
+}
